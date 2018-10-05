60363f0d551d2d9af25333469990b9f591bde216
Eduardo Sánchez <eduardo.sanchez.mata@everis.com>
2018-10-05 14:44:05 +0200

    we test with a sample of Alastria nodes, so my laptop can handle the test and we clean afterwards

--- test/integration.js
+++ test/integration.js
@@ -1,19 +1,29 @@
 #!/usr/bin/env nodejs
 const nodes = require('./nodes').directories()
 const cluster = require('cluster')
+const _ = require('lodash')
+const ondeath = require('death')
+const fs = require('fs')
 require('dotenv').config()
+
+ondeath(function () {
+  fs.readdirSync('.').filter((item) => { return item.startsWith('.node-xmlhttprequest') }).forEach((file) => fs.unlinkSync(file))
+})
+
+let nodeSample = _.shuffle(nodes)
 
 if (cluster.isMaster) {
   if (process.argv.includes('all')) {
-    console.log('testing using all nodes')
-    for (let i = 0; i < nodes.length; i++) {
+    console.log('testing using a sample of Alastria nodes')
+    for (let i = 0; i < Math.min(15, nodeSample.lenght); i++) {
+      console.log('Adding the node', nodeSample[i].entidad, ' to the test')
       const env = {
         NODE_ENV: 'production',
-        RPC_HOST: nodes[i].ip,
+        RPC_HOST: nodeSample[i].ip,
         RPC_PORT: '22000',
         LISTENING_PORT: '21000',
-        INSTANCE_NAME: nodes[i].entidad,
-        CONTACT_DETAILS: nodes[i].contacto,
+        INSTANCE_NAME: nodeSample[i].entidad,
+        CONTACT_DETAILS: nodeSample[i].contacto,
         WS_SERVER: 'ws://127.0.0.1:3000',
         WS_SECRET: 'secret',
         VERBOSITY: '2'
